7bdcdf285b45f84a8a7b97b565324e949e988ea8
bright <brittugu8@gmail.com>
2023-10-29 17:23:52 +0100

    DateFormatter , String and more

---
+++ src/main/kotlin/VisibleNumbers.kt
@@ -0,0 +1,10 @@
+
+fun main(){
+
+    var longnumber = "9765432"
+
+    var fvisble = longnumber.substring(0,3)
+    var svisble = longnumber.substring(5)
+
+    println("${fvisble}***${svisble}")
+}
